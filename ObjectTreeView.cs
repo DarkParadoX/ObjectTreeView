8bf448d267efd0f0f032e1acdb1e0299384c741c
DoX <DarkParadoX00@gmail.com>
2016-12-15 15:58:12 +0300

    Добавлена обработка массивов

--- ObjectTreeView.cs
+++ ObjectTreeView.cs
@@ -11,50 +11,62 @@ namespace ObjectTreeView
     {
         public static string TreeView(this object item, BindingFlags bindingFlags = BindingFlags.Default, TreeMembers treeMembers = TreeMembers.Default )
         {
-            var res = new StringBuilder();
             if (item is IEnumerable)
             {
-                return "[]";
+                var res = new List<string>();
+                foreach (var i in (IEnumerable) item)
+                {
+                    res.Add(treeViewObj(i, bindingFlags, treeMembers));
+                }
+                return "[" + Environment.NewLine +
+                       String.Join("," + Environment.NewLine, res) + Environment.NewLine +
+                       "]";
             }
-            else
+            return treeViewObj(item, bindingFlags, treeMembers);
+        }
+
+        private static string treeViewObj(object item, BindingFlags bindingFlags, TreeMembers treeMembers)
+        {
+            var res = new StringBuilder();
+
+            var itemType = item.GetType();
+            res.AppendLine(item.ToString());
+            res.AppendLine("{");
+
+            if ((treeMembers | TreeMembers.Fields) == TreeMembers.Fields)
             {
-                var itemType = item.GetType();
-
-                if ((treeMembers | TreeMembers.Fields) == TreeMembers.Fields)
+                var fields = itemType.GetFields().ToList();
+                fields.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+                res.AppendLine("Поля:");
+                foreach (var field in fields)
                 {
-                    var fields = itemType.GetFields().ToList();
-                    fields.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
-                    res.AppendLine("Поля:");
-                    foreach (var field in fields)
-                    {
-                        res.AppendLine(String.Format("\t{0}:\t{1}", field.Name, field.GetValue(item) ?? "<null>"));
-                    }
+                    res.AppendLine(String.Format("\t{0}:\t{1}", field.Name, field.GetValue(item) ?? "<null>"));
                 }
-
-                if ((treeMembers | TreeMembers.Properties) == TreeMembers.Properties)
-                {
-                    var properties = itemType.GetProperties().ToList();
-                    properties.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
-                    res.AppendLine("Свойства:");
-                    foreach (var property in properties)
-                    {
-                        res.AppendLine(String.Format("\t{0}:\t{1}", property.Name, property.GetValue(item) ?? "<null>"));
-                    }
-                }
-
-                if ((treeMembers | TreeMembers.Methods) == TreeMembers.Methods)
-                {
-                    var methods = itemType.GetMethods().ToList();
-                    methods.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
-                    res.AppendLine("Методы:");
-                    foreach (var method in methods)
-                    {
-                        res.AppendLine(String.Format("\t{0}\t[{1}]", method.Name, method.ReturnType.FullName));
-                    }
-                }
-
-                return res.ToString();
             }
+
+            if ((treeMembers | TreeMembers.Properties) == TreeMembers.Properties)
+            {
+                var properties = itemType.GetProperties().ToList();
+                properties.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+                res.AppendLine("Свойства:");
+                foreach (var property in properties)
+                {
+                    res.AppendLine(String.Format("\t{0}:\t{1}", property.Name, property.GetValue(item) ?? "<null>"));
+                }
+            }
+
+            if ((treeMembers | TreeMembers.Methods) == TreeMembers.Methods)
+            {
+                var methods = itemType.GetMethods().ToList();
+                methods.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+                res.AppendLine("Методы:");
+                foreach (var method in methods)
+                {
+                    res.AppendLine(String.Format("\t{0}\t[{1}]", method.Name, method.ReturnType.FullName));
+                }
+            }
+            res.Append("}");
+            return res.ToString();
         }
 
         [Flags]
